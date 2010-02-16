322979e2d899ed66c13f8f296dbd340e78c045ba
tom <tom@tom-desktop.(none)>
2010-02-16 17:00:33 +0900

    initial commit

---
+++ lib/Class/Accessor/Lazy.pm
@@ -0,0 +1,32 @@
+package Class::Accessor::Lazy;
+use strict;
+use warnings;
+our $VERSION = '0.01';
+
+1;
+__END__
+
+=head1 NAME
+
+Class::Accessor::Lazy -
+
+=head1 SYNOPSIS
+
+  use Class::Accessor::Lazy;
+
+=head1 DESCRIPTION
+
+Class::Accessor::Lazy is
+
+=head1 AUTHOR
+
+Tom Tsuruhara E<lt>tom.lpsd@gmail.comE<gt>
+
+=head1 SEE ALSO
+
+=head1 LICENSE
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=cut
