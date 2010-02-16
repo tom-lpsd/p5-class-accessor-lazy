ab2a8a3270140f9e6641946d1ae668d6e8889c19
tom <tom@tom-desktop.(none)>
2010-02-16 17:24:28 +0900

    implemented basics

--- lib/Class/Accessor/Lazy.pm
+++ lib/Class/Accessor/Lazy.pm
@@ -1,18 +1,84 @@
 package Class::Accessor::Lazy;
 use strict;
 use warnings;
+use base 'Class::Accessor';
+
 our $VERSION = '0.01';
+
+sub make_accessor {
+    my($class, $field) = @_;
+    my $builder = $class->builder_name_for($field);
+
+    return sub {
+        if (@_ == 1) {
+            if (exists $_[0]->{$field}) {
+                return $_[0]->{$field};
+            }
+            else {
+                return $_[0]->{$field} = $_[0]->$builder;
+            }
+        }
+        return $_[0]->{$field} = $_[1] if @_ == 2;
+        return (shift)->{$field} = \@_;
+    };
+}
+
+
+sub make_ro_accessor {
+    my($class, $field) = @_;
+    my $builder = $class->builder_name_for($field);
+
+    return sub {
+        if (@_ == 1) {
+            if (exists $_[0]->{$field}) {
+                return $_[0]->{$field};
+            }
+            else {
+                return $_[0]->{$field} = $_[0]->$builder;
+            }
+        }
+        my $caller = caller;
+        $_[0]->_croak("'$caller' cannot alter the value of '$field' on objects of class '$class'");
+    };
+}
+
+
+sub make_wo_accessor {
+    my($class, $field) = @_;
+
+    return sub {
+        if (@_ == 1) {
+            my $caller = caller;
+            $_[0]->_croak("'$caller' cannot access the value of '$field' on objects of class '$class'");
+        }
+        else {
+            return $_[0]->{$field} = $_[1] if @_ == 2;
+            return (shift)->{$field} = \@_;
+        }
+    };
+}
+
+sub builder_name_for {
+    my ($class, $field) = @_;
+    return "_build_$field";
+}
 
 1;
 __END__
 
 =head1 NAME
 
-Class::Accessor::Lazy -
+Class::Accessor::Lazy - generate lazy accessors
 
 =head1 SYNOPSIS
 
-  use Class::Accessor::Lazy;
+  use base qw/Class::Accessor::Lazy/;
+  
+  __PACKAGE__->mk_accessors(qw(foo bar));
+  
+  sub _build_foo {
+  
+  }
 
 =head1 DESCRIPTION
 
